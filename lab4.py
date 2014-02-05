21831b5c953b4254126904877bfd9b18766920a5
everystone <eirik.kvarstein@gmail.com>
2014-02-05 12:00:52 +0000

    Lab4 complete

---
+++ lab4.py
@@ -0,0 +1,86 @@
+# -*- coding: latin-1 -*-
+
+#
+#  IS-105 LAB4
+#
+#  lab4.py - kildekode som inneholder studentenes løsning.
+#         
+#
+#
+import sys
+import os
+import subprocess
+import re
+import psutil # Kan installeres med "pip2.7 install psutil"
+
+# Skriv inn fullt navn på gruppemedlemene (erstatte '-' med navn slikt 'Kari Trå')
+gruppe = {  'student1': 'Eirik Kvarstein', \
+}
+
+# Oppgave 1
+# 	Funksjonen lager en strukturert utskrift av resultater fra
+#   kallet psutil.cpu_times(). 
+#	Modulen psutil må være installert.
+#
+#   Utskriften skal være følgende (verdiene skal selvsagt være forskjellige):
+#		user = 3088.16
+#		nice = 0.99
+#		system = 897.37
+#		idle = 72353.81
+#		iowait = 19.29
+#		irq = 6.82
+#		softirq = 3.07
+#		steal = 0.00
+#		guest = 0.00
+#
+def psutils_use():
+	print """
+	Henter lister med systeminformasjon fra /proc og bearbeider disse
+	"""	
+	data = str(psutil.cpu_times(True)) # hente output fra cpu_times inn i string data
+	data2 = data.replace("cputimes(", "") # fjerne cputimes( fra starten av stringen
+	data2 = data2[1:-2] #fjerner første char '[' og de 2 siste '])'
+	list = data2.split(',') # splitte på komma
+
+	for i in list:
+		print i.replace("=", " = ") # legge til space før og etter = og printer
+	
+
+	
+psutils_use()
+
+
+# Oppgave 2
+#	Gitt følgende liste (inn-data):
+# 	proglangs = [('Python', '1989', 'Guido van Rossum'), ('C', '1969', 'Dennis Ritchie'), ('Java/Oak', '1991', 'James Gosling'), ('C++', '1979', 'Bjarne Stroustrup'), ('Ruby', '1991', 'Yukihiro "Matz" Matsumoto'), ('Perl', '1987' , 'Larry Wall'), ('Go/golang', '2007', 'Robert Griesemer, Rob Pike, and Ken Thompson')]
+#
+#	skal funksjonen produsere følgende ut-data:
+#
+#		C ble startet 1969 av Dennis Ritchie.
+#		C++ ble startet 1979 av Bjarne Stroustrup.
+#		Perl ble startet 1987 av Larry Wall.
+#		Python ble startet 1989 av Guido van Rossum.
+#		Java/Oak ble startet 1991 av James Gosling.
+#		Ruby ble startet 1991 av Yukihiro "Matz" Matsumoto.
+#		Go/golang ble startet 2007 av Robert Griesemer, Rob Pike, and Ken Thompson.
+#			
+def print_history(proglangs):
+	# Implementer funksjonen her
+	for line in proglangs:
+		print "\t%s ble startet i %s av %s" %(line[0], line[1], line[2])
+
+
+proglangs = [('Python', '1989', 'Guido van Rossum'), ('C', '1969', 'Dennis Ritchie'), ('Java/Oak', '1991', 'James Gosling'), ('C++', '1979', 'Bjarne Stroustrup'), ('Ruby', '1991', 'Yukihiro "Matz" Matsumoto'), ('Perl', '1987' , 'Larry Wall'), ('Go/golang', '2007', 'Robert Griesemer, Rob Pike, and Ken Thompson')]
+print_history(proglangs)
+
+
+# Standardkall for evalueringen
+print 5*"-" + " Studenter: " + 5*"-"
+for s in gruppe.values():
+	if s is not "-":
+		print s
+
+
+
+
+
